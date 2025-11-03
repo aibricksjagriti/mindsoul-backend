251590229506c6cf7ad0ea88773e7aceb4a8cb7e
Saurabh Mungle <munglesaurabh7284@gmail.com>
2025-11-03 21:08:05 +0530

    Added routes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import authRoutes from "../src/routes/auth.routes.js"
 
 //app config
 const app = express();
@@ -7,5 +8,8 @@ const app = express();
 //middlewares
 app.use(cors());
 app.use(express.json());
+
+//routes
+app.use("/api/auth", authRoutes)
 
 export default app;
