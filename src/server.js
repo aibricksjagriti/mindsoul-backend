744c2f7109cbf774c8ccb3caa70ad478cecbc0e7
Saurabh Mungle <munglesaurabh7284@gmail.com>
2025-11-05 00:53:50 +0530

    firestore connectivity code

--- src/server.js
+++ src/server.js
@@ -1,9 +1,18 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import db from "./config/firestore.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+
+//connect to firestore
+async function testFirestore() {
+  await db.collection("test").doc("check").set({ status: "connected", time: new Date() });
+  console.log("Firestore connection successful");
+}
+
+testFirestore();
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
