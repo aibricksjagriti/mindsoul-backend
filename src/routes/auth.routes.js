484c8b856499357a81ee9bad8ed76f248798887b
Saurabh Mungle <munglesaurabh7284@gmail.com>
2025-11-06 22:33:29 +0530

    Google Sign-In Routes added

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { signup, login } from "../controllers/auth.controllers.js";
+import { googleSignIn } from "../controllers/auth.controllers.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+
+//google sign-in
+router.post("/google", googleSignIn);
 
 export default router;
