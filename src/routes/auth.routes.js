29c749324732be3c5b1617c2357f53ba37d0b4d1
Saurabh Mungle <munglesaurabh7284@gmail.com>
2025-11-07 10:32:21 +0530

    Added openSession route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { signup, login } from "../controllers/auth.controllers.js";
-import { googleSignIn } from "../controllers/auth.controllers.js";
+import { googleSignIn, openSession } from "../controllers/auth.controllers.js";
 
 const router = express.Router();
 
@@ -9,5 +9,8 @@ router.post("/login", login);
 
 //google sign-in
 router.post("/google", googleSignIn);
+
+//firebase session for verified email/password users and google
+router.post("/session", openSession);
 
 export default router;
