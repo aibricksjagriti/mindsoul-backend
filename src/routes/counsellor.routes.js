62b27d0700f5ae905fbb4ce22b8a391f6809f6a4
Saurabh Mungle <munglesaurabh7284@gmail.com>
2025-11-11 23:45:20 +0530

    Added cancel appointment routes

--- src/routes/counsellor.routes.js
+++ src/routes/counsellor.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createAppointment, sendOtp, updateProfile, verifyOtp, listAppointments, getAppointment, updateAppointment } from "../controllers/counsellor.controllers.js";
+import { createAppointment, sendOtp, updateProfile, verifyOtp, listAppointments, getAppointment, updateAppointment, cancelAppointment } from "../controllers/counsellor.controllers.js";
 
 
 const router = express.Router();
@@ -19,6 +19,9 @@ router.get("/list-appointments", listAppointments);
 router.get("/get-appointment", getAppointment);
 
 router.patch("/update-appointment", updateAppointment);
+
+router.patch("/cancel-appointment", cancelAppointment);
+
 
 
 export default router;
