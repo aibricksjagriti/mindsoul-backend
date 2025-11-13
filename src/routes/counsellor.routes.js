cc04eef9fb36c507da12e5408363d0a422789994
Saurabh Mungle <munglesaurabh7284@gmail.com>
2025-11-14 00:12:31 +0530

    Added upload image middleware

--- src/routes/counsellor.routes.js
+++ src/routes/counsellor.routes.js
@@ -1,5 +1,7 @@
 import express from "express";
   import { sendOtp, updateProfile, verifyOtp } from "../controllers/counsellor.controllers.js";
+import { upload } from "../middlewares/uploadImages.js";
+
 
 
 const router = express.Router();
@@ -10,7 +12,7 @@ router.post("/send-otp", sendOtp);
 
 router.post("/verify-otp", verifyOtp);
 
-router.post("/update-profile", updateProfile);
+router.post("/update-profile",upload.single("profileImage"),  updateProfile);
 
 
 
