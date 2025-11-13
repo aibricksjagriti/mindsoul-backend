dff54249d56aab487bf1a92ca11971e0d4ed1268
Saurabh Mungle <munglesaurabh7284@gmail.com>
2025-11-13 13:34:45 +0530

    added express middleware

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import authRoutes from "../src/routes/auth.routes.js"
 import counsellorRoutes from "../src/routes/counsellor.routes.js"
+import appointmentRoutes from "../src/routes/appointment.routes.js"
 
 
 //app config
@@ -10,9 +11,12 @@ const app = express();
 //middlewares
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 //routes
 app.use("/api/auth", authRoutes)
 app.use("/api/counsellor", counsellorRoutes);
+app.use("/api/appointment", appointmentRoutes)
 
 export default app;
