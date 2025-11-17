5dbbbe7c5009dc877f119161d60acf2f6e6b9bf3
Saurabh Mungle <munglesaurabh7284@gmail.com>
2025-11-17 23:47:38 +0530

    Added getAllCounsellors and filterCounsellors routes

--- src/routes/counsellor.routes.js
+++ src/routes/counsellor.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-  import { sendOtp, updateProfile, verifyOtp } from "../controllers/counsellor.controllers.js";
+  import { filterCounsellors, getAllCounsellors, sendOtp, updateProfile, verifyOtp } from "../controllers/counsellor.controllers.js";
 import { upload } from "../middlewares/uploadImages.js";
 import { authenticate } from "../middlewares/auth.middlewares.js";
 
@@ -14,7 +14,11 @@ router.post("/verify-otp", verifyOtp);
 
 router.post("/update-profile", authenticate, upload.single("profileImage"),  updateProfile);
 
+//get counsellors
+router.get("/list", getAllCounsellors);
 
+//filter route for counsellors
+router.get("/filter", filterCounsellors);
 
 
 export default router;
