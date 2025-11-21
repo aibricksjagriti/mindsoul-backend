9d526b9019f4115f06f7e19be765a35fec3c7616
Saurabh Mungle <munglesaurabh7284@gmail.com>
2025-11-22 02:28:36 +0530

    Added timeslots routes

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import authRoutes from "../src/routes/auth.routes.js"
 import counsellorRoutes from "../src/routes/counsellor.routes.js"
 import appointmentRoutes from "../src/routes/appointment.routes.js"
 import userRoutes from "../src/routes/user.routes.js"
+import timeslotRoutes from "../src/routes/timeslot.routes.js";
 import cookieParser from "cookie-parser";
 
 
@@ -22,6 +23,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/counsellor", counsellorRoutes);
 app.use("/api/appointment", appointmentRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/timeslots", timeslotRoutes);
 
 
 export default app;
