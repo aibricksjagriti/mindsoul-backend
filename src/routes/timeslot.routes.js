523a905f2a791e8f6bcbaf7acf2943e0807d5b0b
Saurabh Mungle <munglesaurabh7284@gmail.com>
2025-11-22 02:35:22 +0530

    route file for timeslots controllers

---
+++ src/routes/timeslot.routes.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { authenticate } from "../middlewares/auth.middlewares.js";
+import {
+  getAvailableSlots,
+  generateSlotsForCounsellor,
+  deleteSlots,
+} from "../controllers/timeslot.controllers.js";
+
+const router = express.Router();
+
+//GET  available slots 
+//GET /api/timeslots/counsellor/:id/slots?date=YYYY-MM-DD
+router.get("/counsellor/:id/slots",authenticate, getAvailableSlots);
+
+
+//POST generate slots for a counsellor for a Date
+//POST /api/timeslots/counsellor/:id/slots/generate
+//BODY : { date : "YYYY-MM-DD" }
+router.post("/counsellor/:id/slots/generate",authenticate, generateSlotsForCounsellor);
+
+
+//DELETE all slots for a date
+router.delete("/cousellor/:id/slots", authenticate,  deleteSlots);
+
+export default router;
