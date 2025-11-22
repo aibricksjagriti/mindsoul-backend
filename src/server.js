283c3bef9fe9e1ec4825636a65e2bcbd0e74db52
Saurabh Mungle <munglesaurabh7284@gmail.com>
2025-11-22 19:08:17 +0530

    Added entry route

--- src/server.js
+++ src/server.js
@@ -14,10 +14,12 @@ async function testFirestore() {
     .set({ status: "connected", time: new Date() });
   console.log("Firestore connection successful");
 }
-
-
-
 testFirestore();
+
+//entry route for deployed backend
+app.get("/", (req, res)=> {
+  res.send("Hello from Mindsoul Backend");
+})
 
 app.listen(PORT,"0.0.0.0", () => {
   console.log(`Server running on http://localhost:${PORT}`);
