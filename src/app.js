2c6e79710db09fec586577b45f95eb3c0a3ed2d7
Saurabh Mungle <munglesaurabh7284@gmail.com>
2025-11-25 12:29:45 +0530

    minor issue fixed

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import counsellorRoutes from "./routes/counsellor.routes.js";
 import appointmentRoutes from "./routes/appointment.routes.js";
 import timeslotRoutes from "./routes/timeslot.routes.js";
 // import quoteRoutes from "./routes/quote.routes.js";
-import userRoutes from "./routes/user.routes.js";
+// import userRoutes from "./routes/user.routes.js";
 
 
 //app config
@@ -24,7 +24,7 @@ app.use("/api/counsellor", counsellorRoutes);
 app.use("/api/appointment", appointmentRoutes);
 app.use("/api/timeslots", timeslotRoutes);
 // app.use("/api", quoteRoutes);
-app.use("/api/users", userRoutes);
+// app.use("/api/users", userRoutes);
 
 
 //entry route for deployed backend
