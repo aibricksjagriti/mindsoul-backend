89be61a4cdd153fd1d0d632e5c1766cc32f4fa27
Saurabh Mungle <munglesaurabh7284@gmail.com>
2025-11-25 12:26:46 +0530

    minor issue fixed

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import authRoutes from "./routes/auth.routes.js";
 import counsellorRoutes from "./routes/counsellor.routes.js";
 import appointmentRoutes from "./routes/appointment.routes.js";
 import timeslotRoutes from "./routes/timeslot.routes.js";
-import quoteRoutes from "./routes/quote.routes.js";
+// import quoteRoutes from "./routes/quote.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 
@@ -23,7 +23,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/counsellor", counsellorRoutes);
 app.use("/api/appointment", appointmentRoutes);
 app.use("/api/timeslots", timeslotRoutes);
-app.use("/api", quoteRoutes);
+// app.use("/api", quoteRoutes);
 app.use("/api/users", userRoutes);
 
 
