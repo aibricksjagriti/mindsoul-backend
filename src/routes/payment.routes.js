ca7a9fca5ccc1c348a86d031d22f098e6a5efcd0
Saurabh Mungle <munglesaurabh7284@gmail.com>
2025-12-27 20:09:00 +0530

    added 2 new routes

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,18 +1,29 @@
 import express from "express";
-// import {  createRazorpayOrder_Test } from "../controllers/payment/createOrderController.js";
+import { createRazorpayOrder_Test } from "../controllers/payment/createOrderController.js";
 import { createRazorpayOrder } from "../controllers/payment/createOrderController.js";
 import { authenticate } from "../middlewares/auth.middlewares.js";
 import { verifyRazorpayPayment } from "../controllers/payment/verifyPaymentController.js";
 import { razorpayWebhook } from "../controllers/payment/webhookController.js";
+import {
+  getCounsellorPayments,
+  getUserPayments,
+} from "../controllers/payment/paymentHistoryController.js";
 
 const router = express.Router();
 
 router.post("/create-order", authenticate, createRazorpayOrder);
+router.post("/create-order", createRazorpayOrder_Test);
 
-// router.post("/create-order", createRazorpayOrder_Test);  
+// user payment history
+router.get("/history/user", authenticate, getUserPayments);
 
-router.post("/verify-payment", authenticate, verifyRazorpayPayment);
+// counsellor payment history
+router.get("/history/counsellor", authenticate, getCounsellorPayments);
 
+//verify payment
+router.post("/verify-payment", verifyRazorpayPayment);
+
+//razorpay webhook
 router.post("/webhook", razorpayWebhook);
 
 export default router;
