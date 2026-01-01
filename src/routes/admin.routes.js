adf70235f7b369ab4e36fea793a29e68104c29ec
Saurabh Mungle <munglesaurabh7284@gmail.com>
2026-01-01 12:23:35 +0530

    commented authentication

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { cleanupExpiredAppointments } from "../controllers/cleanup.controllers.js";
-import { authenticate } from "../middlewares/auth.middlewares.js";
-import  { requireAdmin } from "../middlewares/requireAdmin.middlewares.js";
+// import { authenticate } from "../middlewares/auth.middlewares.js";
+// import  { requireAdmin } from "../middlewares/requireAdmin.middlewares.js";
 
 const router = express.Router();
 
 //Protected route
-router.post("/cleanup-expired-appointments", authenticate, requireAdmin,cleanupExpiredAppointments);
+router.post("/cleanup-expired-appointments",cleanupExpiredAppointments);
 
 export default router;
+  
